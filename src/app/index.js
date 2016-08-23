1d34a5a9041ad27d1238abe8fd884afdcb36a9e7
Maxime Guedj <max.guedj13@gmail.com>
2016-08-23 16:42:48 +0200

    Add api to rest-service

--- src/app/index.js
+++ src/app/index.js
@@ -2,12 +2,12 @@ const vhost = require('vhost');
 const config = require('config');
 const devService = require('spred-dev-service');
 const loginService = require('spred-login-service');
-//const apiService = require('sharemyscreen-api-service');
+const apiService = require('spred-api-service');
 
 function registerApp (masterApp) {
   masterApp.use(vhost(config.get('devService.url'), devService.getApp()));
   masterApp.use(vhost(config.get('loginService.url'), loginService.getApp(true)));
-//  masterApp.use(vhost(config.get('apiService.url'), apiService.getApp(true)));
+  masterApp.use(vhost(config.get('apiService.url'), apiService.getApp(true)));
 }
 
 module.exports.registerApp = registerApp;
